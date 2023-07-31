2060491a9a3c25e51bccc15b0d271c4126d32565
heliandro <heliandro.l@gmail.com>
2023-07-31 07:37:50 -0300

    feat: add greeting endpoint

--- src/main/java/br/com/heliandro/restwithspringboot/RestWithSpringBootApplication.java
+++ src/main/java/br/com/heliandro/restwithspringboot/RestWithSpringBootApplication.java
@@ -2,8 +2,10 @@ package br.com.heliandro.restwithspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class RestWithSpringBootApplication {
 
 	public static void main(String[] args) {
